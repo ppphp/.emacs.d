f7e24d3e68880fabc333876364694f92af115205
ppphp <kevinniub@gmail.com>
2018-06-28 19:07:06 +0000

    update

--- init.el
+++ init.el
@@ -1,4 +1,6 @@
-;; my theme
+(package-initialize)
+
+;; theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 ;;(load-theme 'dracula t)
 (load-theme 'atom-one-dark t)
@@ -6,18 +8,34 @@
 ;; close welcome
 (setq inhibit-startup-message t)
 
-;; line number
-(global-linum-mode t)
+;; overall setting
+(global-linum-mode t) ;; show line number
+(scroll-bar-mode -1) ;; hide scroll bar
+
+
+;; encoding && font https://www.reddit.com/r/emacs/comments/5twcka/which_font_do_you_use/
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8-unix)
+(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-12"))
+
+;; multiterm
+(add-to-list 'load-path "~/.emacs.d/multi-term")
+(require 'multi-term)
+(setq multi-term-program "/bin/bash")
+(global-set-key [f12] 'multi-term-dedicated-toggle)
+
+
 
 ;; directory tree
 (add-to-list 'load-path "~/.emacs.d/neotree")
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
-(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 
-;; shell TODO
-;;(add-to-list 'load-path "~/.emacs.d/neotree")
-;;(require 'multi-term)
+
+;; project
+(add-to-list 'load-path "~/.emacs.d/projectile")
+(require 'projectile)
+(projectile-mode)
 
 
 ;; org mode
@@ -25,18 +43,4 @@
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-switchb)
-
-
-
-(require 'package)
-;;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-;;(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-
-
-(package-initialize)
-
-
-
-
-
 
