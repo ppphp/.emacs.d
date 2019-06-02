f68c120c299435001c182c2d372995d3fe1b427a
ppphp <kevinniub@gmail.com>
2019-06-03 00:43:53 +0800

    update mess

--- init.el
+++ init.el
@@ -1,54 +1,56 @@
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/modules/themes")
 (load-theme 'atom-one-dark t)
 
-(add-to-list 'load-path "~/.emacs.d/projectile")
+;; global projectile init
+(add-to-list 'load-path "~/.emacs.d/modules/projectile")
 (require 'projectile)
 (projectile-mode +1)
 
-(add-to-list 'load-path "~/.emacs.d/ghub")
-(add-to-list 'load-path "~/.emacs.d/magit-popup")
-(add-to-list 'load-path "~/.emacs.d/with-editor")
-(add-to-list 'load-path "~/.emacs.d/dash")
-(add-to-list 'load-path "~/.emacs.d/magit/lisp")
+;; global magit init
+(add-to-list 'load-path "~/.emacs.d/modules/ghub")
+(add-to-list 'load-path "~/.emacs.d/modules/magit-popup")
+(add-to-list 'load-path "~/.emacs.d/modules/with-editor")
+(add-to-list 'load-path "~/.emacs.d/modules/dash")
+(add-to-list 'load-path "~/.emacs.d/modules/magit/lisp")
 (require 'magit)
 
-(add-to-list 'load-path "~/.emacs.d/popup-el")
-(add-to-list 'load-path "~/.emacs.d/emacs-async")
-(add-to-list 'load-path "~/.emacs.d/helm")
+;; global helm init
+(add-to-list 'load-path "~/.emacs.d/modules/popup-el")
+(add-to-list 'load-path "~/.emacs.d/modules/emacs-async")
+(add-to-list 'load-path "~/.emacs.d/modules/helm")
 (require 'helm)
 (require 'helm-config)
 (helm-mode 1)
 
-(add-to-list 'load-path "~/.emacs.d/company-mode")
+;; global company init
+(add-to-list 'load-path "~/.emacs.d/modules/company-mode")
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 (setq company-minimum-prefix-length 1)
 
-(add-to-list 'load-path "~/.emacs.d/markdown-mode")
-(add-to-list 'load-path "~/.emacs.d/spinner.el")
-(add-to-list 'load-path "~/.emacs.d/ht.el")
-(add-to-list 'load-path "~/.emacs.d/s")
-(add-to-list 'load-path "~/.emacs.d/f.el")
-(add-to-list 'load-path "~/.emacs.d/dash")
-(add-to-list 'load-path "~/.emacs.d/lsp-mode")
+;; lsp-mode init
+(add-to-list 'load-path "~/.emacs.d/modules/markdown-mode")
+(add-to-list 'load-path "~/.emacs.d/modules/spinner.el")
+(add-to-list 'load-path "~/.emacs.d/modules/ht.el")
+(add-to-list 'load-path "~/.emacs.d/modules/s")
+(add-to-list 'load-path "~/.emacs.d/modules/f.el")
+(add-to-list 'load-path "~/.emacs.d/modules/dash")
+(add-to-list 'load-path "~/.emacs.d/modules/lsp-mode")
 (require 'lsp-mode)
-(lsp-register-client
- (make-lsp-client :new-connection (lsp-stdio-connection "pyls")
-		  :major-modes '(python-mode)
-		  :server-id 'pyls
-		  )
- )
+(require 'lsp)
+(require 'lsp-rust)
+(require 'lsp-pyls)
+(require 'lsp-go)
+(add-hook 'python-mode-hook #'lsp-python-enable)
+(add-hook 'go-mode-hook #'lsp-go-enable)
 
-(lsp-register-client
- (make-lsp-client :new-connection (lsp-stdio-connection "gopls")
-		  :major-modes '(go-mode)
-		  :server-id 'gopls
-		  )
- )
-
-(add-to-list 'load-path "~/.emacs.d/lsp-ui")
+(add-to-list 'load-path "~/.emacs.d/modules/lsp-ui")
 (require 'lsp-ui)
+(require 'lsp-ui-imenu)
+(setq lsp-ui-sideline-ignore-duplicate t)
+(add-hook 'lsp-mode-hook 'lsp-ui-mode)
 
-(add-to-list 'load-path "~/.emacs.d/company-lsp")
+
+(add-to-list 'load-path "~/.emacs.d/modules/company-lsp")
 (require 'company-lsp)
 (push 'company-lsp company-backends)
