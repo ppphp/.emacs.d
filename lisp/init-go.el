1ac7f15f81b883473f02ff9ea794b2bc2439a95e
fengche <2395434298@qq.com>
2019-06-11 20:59:36 +0800

    update

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -3,6 +3,14 @@
 ;;; Code:
 ;(add-to-list 'load-path "~/.emacs.d/modules/go-mode.el")
 ;(autoload 'go-mode "go-mode" nil t)
+
+(defun go-mode
+    ()
+  "My go mode."
+  ()
+  )
+
+
 (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
 (provide 'init-go)
 ;;; init-go.el ends here
