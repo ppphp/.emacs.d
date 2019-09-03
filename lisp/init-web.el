6f6edaa0efd9f7a1c481b43e96aa5f0f160cc5ae
fengche <2395434298@qq.com>
2019-09-03 22:11:37 +0800

    update

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -5,9 +5,10 @@
 (require 'web-mode)
 
 (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+(setq lsp-clients-javascript-typescript-server ("~" ".yarn" "bin" "typescript-language-server"))
+(add-hook 'js-mode-hook #'lsp)
 (add-hook 'typescript-mode-hook #'lsp)
 
-(setq lsp-clients-typescript-server (f-join user-emacs-directory "bin" "typescript-language-server"))
 
 (provide 'init-web)
 ;;; init-web.el ends here
