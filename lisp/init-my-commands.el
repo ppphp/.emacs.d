8661d53c2bd389d4c7635c29583a63f301cda8a5
fengche <2395434298@qq.com>
2020-08-27 12:41:38 +0800

    update

--- lisp/init-my-commands.el
+++ lisp/init-my-commands.el
@@ -8,5 +8,10 @@
   (interactive)
   (call-process-shell-command "make" nil t t "-C" "~/.emacs.d"))
 
+(defun jump-to-buffer (name)
+  "Jump to a buffer with NAME,"
+  (interactive)
+  )
+
 
 ;;; init-my-commands.el ends here
