9ddb361e75456f68496097f8dbed754f8c2765ff
fengche <2395434298@qq.com>
2020-09-21 15:12:48 +0800

    update

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -4,14 +4,30 @@
 
 ;; major mode
 
+(require 'pretty-hydra)
 (use-package go-mode
+  :if (executable-find "go")
+  :pretty-hydra
+  (()
+   ("gen test"
+   (("a" go-gen-test-all "gen")
+    ("e" go-gen-test-exported "function")
+    ("b" go-gen-test-dwim "dwim"))
+   "run test"
+   (("t" go-test-current-test "function")
+    ("f" go-test-current-file "file")
+    ("p" go-test-current-project "project"))
+  "play"
+   (("l" go-playground))
+  "coverage"
+   (("c" go-coverage))))
   :config
   (use-package flycheck-golangci-lint
     :after (flycheck)
     :hook (flycheck-mode . flycheck-golangci-lint-setup)
     :custom
     (flycheck-golangci-lint-fast t)
-    (flycheck-golangci-lint-enable-all t))
+    (flycheck-golangci-lint-enable-all nil))
   (use-package go-fill-struct)
   (use-package go-gen-test
     :custom
@@ -27,23 +43,6 @@
 (require 'formatters-goimports)
 
 (add-hook 'go-mode-hook (lambda () (add-hook 'before-save-hook #'formatters)))
-
-(require 'major-mode-hydra)
-
-(major-mode-hydra-define go-mode nil
-  ("gen test"
-   (("a" go-gen-test-all "gen")
-    ("e" go-gen-test-exported "function")
-    ("b" go-gen-test-dwim "dwim"))
-   "run test"
-   (("t" go-test-current-test "function")
-    ("f" go-test-current-file "file")
-    ("p" go-test-current-project "project"))
-  "play"
-   (("l" go-playground))
-  "coverage"
-   (("c" go-coverage))))
-
 
 ;; debug protocol
 (require 'dap-go)
--- lisp/init-org.el
+++ lisp/init-org.el
@@ -2,40 +2,59 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'org)
+(use-package org
+  :config
+  ;; beautify
+  (use-package org-bullets
+    :hook (org-mode . org-bullets-mode))
 
-;; beautify
-(require 'org-bullets)
-(add-hook 'org-mode-hook #'org-bullets-mode)
+  ;; shortcut
+  (use-package org-tempo)
 
-;; shortcut
-(require 'org-tempo)
+  ;; journal
+  (use-package org-journal
+    :custom
+    (org-journal-dir "~/.emacs.d/notes/journal")
+    (org-journal-date-format "%d %B %Y"))
 
-;; babel
-(require 'ob-async)
-(require 'ob-elixir)
-(require 'ob-go)
-;(require 'ob-ipython)
-(require 'ob-lilypond)
-(require 'ob-mermaid)
-(require 'ob-rust)
-(require 'ob-swift)
+  ;; babel
+  (use-package ob
+    :config
+    (defvar load-language-list '((emacs-lisp . t)
+				 (perl . t)
+				 (python . t)
+				 (ruby . t)
+				 (js . t)
+				 (css . t)
+				 (sass . t)
+				 (C . t)
+				 (java . t)
+				 (plantuml . t)))
+    (use-package ob-go
+      :init (cl-pushnew '(go . t) load-language-list))
+    (use-package ob-rust
+      :init (cl-pushnew '(rust . t) load-language-list))
+    ;(use-package ob-ipython
+    ;  :if (executable-find "jupyter")     ; DO NOT remove
+    ;  :init (cl-pushnew '(ipython . t) load-language-list))
+    ;; Use mermadi-cli: npm install -g @mermaid-js/mermaid-cli
+    (use-package ob-mermaid
+      :init (cl-pushnew '(mermaid . t) load-language-list))
+    (use-package ob-elixir
+      :init (cl-pushnew '(rust . t) load-language-list))
+    (use-package ob-lilypond
+      :init (cl-pushnew '(rust . t) load-language-list))
+    (use-package ob-swift
+      :init (cl-pushnew '(rust . t) load-language-list))
+    (org-babel-do-load-languages 'org-babel-load-languages
+				 load-language-list)
+    (use-package ob-async))
 
-;; journal
-(setq org-journal-dir "~/.emacs.d/notes/journal")
-(setq org-journal-date-format "%d %B %Y")
-(require 'org-journal)
-
-;; agenda
-(setq org-agenda-files (list (f-join user-emacs-directory "local" "agenda")))
-
-(setq org-log-done t)
-
-(setq org-agenda-include-diary t)
-(setq org-agenda-diary-file (f-join user-emacs-directory "local" "diary.org"))
-
+  
 (setq calendar-longitude 121.4737)
 (setq calendar-latitude 31.2304)
+
+(setq org-log-done t)
 
 (defun diary-sunrise ()
   (let ((dss (diary-sunrise-sunset)))
@@ -64,18 +83,47 @@
       (goto-char start)
       (capitalize-word 1)
       (buffer-substring start end))))
+  ;; agenda
+  (use-package org-agenda
+    :custom
+    (org-agenda-files (list (f-join user-emacs-directory "local" "agenda")))
+    (org-agenda-include-diary t)
+    (org-agenda-diary-file (f-join user-emacs-directory "local" "diary.org"))
+    (org-agenda-time-grid (quote (((daily today require-timed)
+				   (800 1000 1200 1400 1600 1800 2000)
+				   "......" "----------------"))))
+  
+)
 
-(setq org-agenda-time-grid (quote (((daily today require-timed)
-                                    (800 1000 1200 1400 1600 1800 2000)
-                                    "......" "----------------"))))
-
-;; github export
-(require 'ox-gfm)
-(add-to-list 'org-export-backends 'md)
+  (use-package org-pomodoro)
+  
+  ;; github export
+  (use-package ox-gfm
+    :config
+    (add-to-list 'org-export-backends 'md)))
 
 ;; org-roam
-(require 'org-roam)
-(setq org-roam-directory (f-join user-emacs-directory "notes" "org-roam"))
+(use-package org-roam
+  :hook
+  (after-init . org-roam-mode)
+  :custom
+  (org-roam-directory (f-join user-emacs-directory "notes" "org-roam")))
+
+  (use-package org-roam-server
+    :functions xwidget-buffer xwidget-webkit-current-session
+    :hook (org-roam-server-mode . org-roam-server-browse)
+    :init
+    (defun org-roam-server-browse ()
+      (when org-roam-server-mode
+        (let ((url (format "http://%s:%d" org-roam-server-host org-roam-server-port)))
+          (if (featurep 'xwidget-internal)
+              (progn
+                (xwidget-webkit-browse-url url)
+                (let ((buf (xwidget-buffer (xwidget-webkit-current-session))))
+                  (when (buffer-live-p buf)
+                    (and (eq buf (current-buffer)) (quit-window))
+                    (pop-to-buffer buf))))
+            (browse-url url))))))
 
 (provide 'init-org)
 ;;; init-dashboard.el ends here
