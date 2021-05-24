179cf1fd8764ba3feedbafe0e0033f07c4a67344
fengche <2395434298@qq.com>
2021-05-24 13:49:16 +0800

    update

--- lisp/init-treemacs.el
+++ lisp/init-treemacs.el
@@ -6,8 +6,7 @@
   :commands (treemacs)
   :after (all-the-icons f)
   :custom
-  (treemacs-persist-file (f-join user-emacs-directory "local" "treemacs-persist")
-			 treemacs-position 'right)
+  (treemacs-persist-file (f-join user-emacs-directory "local" "treemacs-persist"))
   :config
   (use-package treemacs-projectile)
   (use-package treemacs-magit)
@@ -17,6 +16,7 @@
   (use-package doom-themes-ext-treemacs
     :config
     (doom-themes-treemacs-config)))
+  (setq treemacs-position 'right)
 
  (provide 'init-treemacs)
 ;;; init-treemacs.el ends here
